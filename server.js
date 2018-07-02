e2cd3fa3ba566404cad49f42662c6586d826e57c
Datta973 <mvdatta3@gmail.com>
2018-07-02 18:43:11 +0530

    yayyy

--- server.js
+++ server.js
@@ -129,6 +129,9 @@ io.on('connection', function (socket) {
     // ]);
 
     // circles[socket.id] = new C(new V(), 32);
+    socket.on("yay",function(data){
+        socket.emit("cool","yessssssss,it worked!!! "+data);
+    })
 
     socket.on("start_game", function (data) {
 
